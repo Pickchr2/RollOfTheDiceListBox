a0cf68c1b42312b4d219acab4ff7e20e3bd8bd7f
Pickchr2 <Cpickens1270@gmail.com>
2023-03-26 12:07:38 -0600

    Code clear and exit buttons

--- RollOfTheDiceListBox/RollOfTheDiceListBox.vb
+++ RollOfTheDiceListBox/RollOfTheDiceListBox.vb
@@ -8,5 +8,12 @@ Option Explicit On
 Option Strict On
 
 Public Class RollOfTheDiceListBox
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        RollResultListBox.Items.Clear()
+        RollButton.Select()
+    End Sub
 End Class
